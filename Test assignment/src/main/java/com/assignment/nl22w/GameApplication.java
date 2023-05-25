d7f730a3d7498dcfc6e1ee35c94cc04becb4d727
LadvikM <Ladvik@gmail.com>
2023-05-25 19:30:40 +0300

    Refactoring

--- Test assignment/src/main/java/com/assignment/nl22w/GameApplication.java
+++ Test assignment/src/main/java/com/assignment/nl22w/GameApplication.java
@@ -1,22 +1,12 @@
 package com.assignment.nl22w;
 
-import com.assignment.nl22w.game.impl.GameImpl;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.core.io.ClassPathResource;
-import org.springframework.core.io.Resource;
-
-import java.io.*;
 
 @SpringBootApplication
 public class GameApplication {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args){
 		SpringApplication.run(GameApplication.class, args);
-		Resource resource = new ClassPathResource("NoMap.txt");
-		GameImpl game = new GameImpl();
-		game.escapeFromTheWoods(resource);
-
-
 	}
 }
